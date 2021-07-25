bd67c7a914a2aebb911a9f2211fece5a575f6b34
Kuzmin Roman <archer039@gmail.com>
2021-07-25 08:54:58 +0200

    second test auto format before commit

--- frontend/controllers/TestModelController.php
+++ frontend/controllers/TestModelController.php
@@ -12,15 +12,22 @@ use frontend\models\Category;
 
 class TestModelController extends Controller
 {
-    public function actionSave(){
+    public function actionSave()
+    {
         $category = new Category();
         $category->title = 'New category';
+
         $category->save();
     }
+
     public function actionShow(){
         $category = Category::find()->one();
-        if ($category){
-            return $this->render('showCategory', ['categoryTitle' => $category->title]);
+        if
+        ($category){
+            return $this->render(
+                'showCategory',
+                ['categoryTitle' => $category->title]
+            );
         }
     }
 }

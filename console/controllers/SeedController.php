cd6a7724d20889a71255484247b2047af63e8cb8
Roman Kuzmin <archer039@gmail.com>
2021-08-15 01:38:38 +0300

    добавленна консольная команда seed

--- console/controllers/SeedController.php
+++ console/controllers/SeedController.php
@@ -5,6 +5,8 @@ namespace console\controllers;
 use Yii;
 use yii\console\Controller;
 use yii\db\Exception;
+use yii\helpers\Console;
+use yii\helpers\FileHelper;
 
 class SeedController extends Controller
 {
@@ -13,15 +15,36 @@ class SeedController extends Controller
      */
     public function actionIndex()
     {
-        $db = Yii::$app->db;
-        if (!$db) {
-            echo 'Нет подключения к БД';
-            exit;
+        $pathExportData = __DIR__ . '/../../sqlExportData';
+        if (file_exists($pathExportData)) {
+            if (is_dir($pathExportData)) {
+                $files = FileHelper::findFiles($pathExportData, ['only' => ['*.sql']]);
+                if (!$files) {
+                    echo 'В указанной директории нет SQL файлов!';
+                    exit;
+                }
+
+                foreach ($files as $file) {
+                    echo $file . PHP_EOL;
+                }
+                if (Console::confirm('Подтвердите выбранные файлы')) {
+                    $db = Yii::$app->db;
+
+                    if (!$db) {
+                        echo 'Нет подключения к БД';
+                        exit;
+                    }
+
+                    foreach ($files as $file) {
+                        $sql = file_get_contents($file);
+                        $db->createCommand($sql)->execute();
+                    }
+
+                    echo 'БД наполнена';
+                }
+
+                exit;
+            }
         }
-
-        $sql = file_get_contents(__DIR__ . '/../../sqlExportData/01_categories.sql');
-
-        $db->createCommand($sql)->execute();
-        echo 'БД наполнена';
     }
 }

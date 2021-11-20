fec83d598ec0a7283fcf9709174c452b5974e18f
Roman Kuzmin <archer039@gmail.com>
2021-11-20 12:38:19 +0200

    fix task/view

--- frontend/views/tasks/view.php
+++ frontend/views/tasks/view.php
@@ -146,3 +146,6 @@ TaskViewAsset::register($this);
         myMap.geoObjects.add(myGeoObject);
     }
 </script>
+<?php if (Yii::$app->user->id === $task->customer_id || Yii::$app->user->id === $task->executor_id): ?>
+    <script src="/js/messenger.js"></script>
+<?php endif; ?>
